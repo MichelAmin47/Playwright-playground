772c488f0bdee2273ee387de924d86921cf53a52
Michel Lalmohamed <michellalmohamed@valori.nl>
2022-09-29 15:39:05 +0200

    code for the workshop

--- TypeScript/pages/login-page.ts
+++ TypeScript/pages/login-page.ts
@@ -8,16 +8,10 @@ export class LoginPage {
 
     constructor(page: Page) {
         this.page = page;
-        this.emailField = page.locator('form#login-form input[type="email"]');
-        this.passwordField = page.locator('form#login-form input[type="password"]');
-
-        // locater filtering / element child
-        this.signInButton = page.locator('footer', { has: page.locator('button', { hasText: 'Sign in' }) })
+        this.emailField = page.locator(' ');
     }
 
     async signIn() {
-        await this.emailField.type('testerino@tester.com');
-        await this.passwordField.type('1qazxsw2');
-        await this.signInButton.click();
+
     }
 }
--- TypeScript/tests/login.spec.ts
+++ TypeScript/tests/login.spec.ts
@@ -3,33 +3,9 @@ import {HomePage} from "../pages/home-page";
 import { LoginPage } from '../pages/login-page';
 
 test('User logs in to the MyAccount page', async ({page }) => {
-    await page.goto('https://webshop.mobiletestautomation.nl/');
 
-    await expect(page).toHaveTitle('Valori Automation Practice Shop');
-
-    await page.locator('text=Sign in').click();
-
-    await page.locator('form#login-form input[type="email"]').type('testerino@tester.com');
-    await page.locator('form#login-form input[type="password"]').type('1qazxsw2');
-    await page.locator('form#login-form button[type="submit"]').click();
-
-    await expect(page.locator('section#main h1')).toContainText('Your account');
 });
 
 test('User logs in to the MyAccount page with the Page Object Model', async ({page }) => {
-    const homePage = new HomePage(page);
-    await homePage.goto();
-    await homePage.goSignIn();
 
-    const loginPage = new LoginPage(page);
-    await loginPage.signIn();
-
-    // CSS combined with text locator!
-    await expect(page.locator('section#main h1')).toContainText('Your account');
-
-    console.log(await page.locator('h1:has-text("Your account")').textContent())
-    await expect(page.locator('h1:has-text("Your account")')).toBeVisible();
-
-    console.log(await page.locator('section#main >> text=Your account').allInnerTexts());
-    await expect(page.locator('section#main >> text=Your account')).toBeVisible();
 });
